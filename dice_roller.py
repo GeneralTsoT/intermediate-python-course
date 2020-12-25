b87dee10037828ed423f3b402a52d15e4f5124aa
Goblin <gentso11@protonmail.com>
2020-12-25 15:05:56 -0500

    complte//

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 import random
 def main():
  
- dice_rolls = 4
+ dice_rolls = int(input('How mony dice would you like?'))
+ dice_sides = int(input('sO how many sides?'))
  dice_sum = 0
  for i in range(0,dice_rolls):
-  roll= random.randint(1,6)
+  roll= random.randint(1,dice_sides)
   dice_sum = dice_sum + roll
-  print(f"You rolled a {roll}")
+  if roll == 1:
+   print (f'you rolled a {roll}! Critical fail')
+  elif roll == dice_sides:
+   print(f'you rolled a {roll}! Critical Success!!')
+  else:
+   print(f"You rolled a {roll}")
  print(f"you have rolled a total of {dice_sum}")
 
 if __name__== "__main__":
